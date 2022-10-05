6939b0e7e3911db26088c32e5c5145209beeec13
Alonezkly <87901345+Alonezkly@users.noreply.github.com>
2022-10-05 09:34:42 +0700

    Add files via upload

---
+++ tools.php
@@ -0,0 +1,88 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=0.75, shrink-to-fit=no">
+   <meta name="description" content="Fathan Raffi Tools Online">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+   <link href="https://fonts.googleapis.com/css?family=Mali:400i,700i" rel="stylesheet" type="text/css">
+   <link rel="icon" href="https://k.top4top.io/p_1789r0r520.png">
+   <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.1/css/all.css' integrity='sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr' crossorigin='anonymous'>
+   <title>Fathan Raffi Tools</title>
+   <style>
+   	html,body,.container{
+   		background: url(https://k.top4top.io/p_1790gymt50.png) no-repeat center center/cover;
+   		position: fixed;
+   		margin: auto;
+   		height: 100%;
+   		top: 0; bottom: 0; left: 0; right:0;
+   	}
+   	.page{
+   		position: absolute;
+   		margin: auto;
+   		height: 50%;
+   		top: 0; bottom: 0; left: 0; right:0;
+   	}
+   	.mek{
+   		background-color:rgba(0,0,0,0.7);
+   	}
+   	h1{
+   		transform:rotate(-12deg); 
+   		-moz-transform:rotate(-12deg); 
+   		-webkit-transform:rotate(-12deg); 
+   		-o-transform:rotate(-12deg);
+   		font-family: "Mali";
+   		font-size: 3em;
+   		font-weight: bold;
+   		margin-bottom: 15%;
+           word-spacing: 10px;
+       }
+
+   	h5{
+   		font-family: "Mali";
+   	}
+   	.logo{
+   		text-decoration: none;
+   		color: white;
+   		font-size: 22px;
+   		margin: 10px;
+   	}
+   	.logo:hover{
+   		text-decoration: none;
+   		color: white;
+     		cursor: pointer;
+   	}
+   	.hastag{
+   		text-decoration: none;
+   		color: white;
+   	}
+   </style>
+ </head>
+
+ 
+
+ <body>
+ 	
+   <div class="container">
+   	<div class="page">
+   		<h1 class="text-center text-white"> Tools Online <br> Fathan Raffi</h1>
+   		
+   		<div class="mek">
+   		<h5 class="text-center text-white mb-3">#Fathan Raffi
+               
+               <hr> 
+               <a href="https://www.youtube.com/channel/UCT9DNdI2P4bsIRYBKPth7Pg">Youtube</a> |
+               <a href="https://fathanraffi.ga/jso.php" target="_blank">JSO</a> | 
+               <a href="https://fathanraffi.ga/csrf.php" target="_blank">CSRF Online</a> | 
+               <a href="https://fathanraffi.ga/clickjeking.php" target="_blank">Click Jacking</a> |
+               <a href="https://fathanraffi.ga/kalkulator.php" target="_blank">Kalkulator Online</a> |
+               <a href="https://fathanraffi.ga/gameroket.php" target="_blank">Game Roket</a> |
+               <a href="https://fathanraffi.ga/Bot.html" target="_blank">Bot Nulis</a> |
+               </div>
+   
+
+</html>
+<!-- U.M7-->
